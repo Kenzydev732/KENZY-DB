fdc4ed03633c3f982b421e3f9988f23bd81dcbd8
Kenzydev732 <waradanis336@gmail.com>
2026-02-05 04:13:07 +0700

    install.sh

--- install.sh
+++ install.sh
@@ -1,2 +1,22 @@
 #!/bin/bash
-echo "Install script berjalan..."
+clear
+
+echo "==============================="
+echo "     KENZY-DB INSTALLER"
+echo "==============================="
+echo "1. Install MySQL"
+echo "0. Keluar"
+echo "==============================="
+read -p "Pilih: " pilih
+
+case $pilih in
+  1)
+    bash mysql.sh
+    ;;
+  0)
+    exit
+    ;;
+  *)
+    echo "Pilihan tidak valid"
+    ;;
+esac
